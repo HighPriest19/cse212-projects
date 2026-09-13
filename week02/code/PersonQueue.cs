181d15d885f8b8ba2028235756b5d6194787cb3e
HighPriest19 <kryptonolagunju@gmail.com>
2026-09-13 18:22:26 +0100

    commit

--- week02/code/PersonQueue.cs
+++ week02/code/PersonQueue.cs
@@ -1,6 +1,3 @@
-/// <summary>
-/// A basic implementation of a Queue
-/// </summary>
 public class PersonQueue
 {
     private readonly List<Person> _queue = new();
@@ -10,14 +7,19 @@ public class PersonQueue
     /// <summary>
     /// Add a person to the queue
     /// </summary>
-    /// <param name="person">The person to add</param>
     public void Enqueue(Person person)
     {
-        _queue.Insert(0, person);
+        // FIX: Change Insert(0, person) to Add(person) so it behaves like a FIFO queue instead of LIFO
+        _queue.Add(person);
     }
 
     public Person Dequeue()
     {
+        if (_queue.Count == 0)
+        {
+            throw new InvalidOperationException("No one in the queue.");
+        }
+
         var person = _queue[0];
         _queue.RemoveAt(0);
         return person;
@@ -25,7 +27,7 @@ public class PersonQueue
 
     public bool IsEmpty()
     {
-        return Length == 0;
+        return _queue.Count == 0;
     }
 
     public override string ToString()
